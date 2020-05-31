c6969f151c7f4690c5078ec00b3b288e6c4049e1
20chan <2@0chan.dev>
2020-05-31 23:18:37 +0900

    init commit

---
+++ LOLFix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace LOLFix {
+    class Program {
+        static void Main(string[] args) {
+            var processes = Process.GetProcessesByName("LeagueClientUxRender");
+            var target = processes[0].WorkingSet64 < processes[1].WorkingSet64 ? processes[1] : processes[0];
+            target.Kill();
+        }
+    }
+}
